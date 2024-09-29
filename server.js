86199e29bb04f4719f9b6882b1e8acb488a77809
TsytovychNataliya-FSO <NTsytovych@student.fullsail.edu>
2024-09-29 14:40:19 -0500

    Updating the server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors"); // Import CORS
 const app = express();
 const routes = require("./app/routes/index"); // Correct path to index.js
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Use CORS middleware to allow cross-origin requests
+app.use(cors()); // Add this line
 
 // Use the routes defined in routes/index.js
 app.use("/", routes);
@@ -13,6 +17,7 @@ const connectDB = require("./app/db/config");
 connectDB();
 
 // Start server
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+const PORT = process.env.PORT || 5001; // Use environment variable for port
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
